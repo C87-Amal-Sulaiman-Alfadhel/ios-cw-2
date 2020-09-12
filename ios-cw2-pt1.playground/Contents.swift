e0a5380157afc960aad2d4752742dd15c72057d0
C87-Amal-Sulaiman-Alfadhel <70811155+C87-Amal-Sulaiman-Alfadhel@users.noreply.github.com>
2020-09-12 21:29:56 +0300

    cw-2 done

---
+++ ios-cw2-pt1.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+
+let family = ["Amal", "Sulaiman", "Abdullah", "Bodour", "Heba", "Sarah"]
+var evens = [2, 4, 6, 8, 10, 12]
+var grades = [99.8, 99.8, 97.2]
+var trueFalse = [true, true, true, false]
+print("Family: ", family[0], family[5])
+print("Evens: ", evens[0], evens[5])
+print("Grades: ", grades[0], grades[2])
+print("TrueFalse: ", trueFalse[0], trueFalse[3])
